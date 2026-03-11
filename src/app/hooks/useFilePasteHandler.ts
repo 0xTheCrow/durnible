a55746bead0df9ace3662de13321c7ce3a5bf39b
0xTheCrow <cooperdombeck@gmail.com>
2026-03-10 19:36:27 -0500

    dubug

--- src/app/hooks/useFilePasteHandler.ts
+++ src/app/hooks/useFilePasteHandler.ts
@@ -5,15 +5,8 @@ export const useFilePasteHandler = (onPaste: (file: File[]) => void): ClipboardE
   useCallback(
     (evt) => {
       console.log('[paste]', {
-        filesCount: evt.clipboardData?.files?.length,
-        itemsCount: evt.clipboardData?.items?.length,
-        items: evt.clipboardData?.items
-          ? Array.from(evt.clipboardData.items).map((item) => ({
-              kind: item.kind,
-              type: item.type,
-            }))
-          : null,
-        types: evt.clipboardData?.types,
+        html: evt.clipboardData?.getData('text/html'),
+        text: evt.clipboardData?.getData('text/plain'),
       });
       const files = getDataTransferFiles(evt.clipboardData);
       console.log('[paste] files:', files);
